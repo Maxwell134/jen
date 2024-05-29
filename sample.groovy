9647769dc831da8fbe3223585bf39439c6d9548b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-05-30 00:27:51 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,4 +1,11 @@
 def hello(image, tag, port) {
+    def container = 'docker ps -a'
+    def output = sh(script: container, returnStdout: true).trim()
+    
+    if (output) {
+        sh "docker rm -f $(docker ps -qa)"
+    }
+    
     sh "docker run -d -p ${port}:80 ${image}:${tag}"
 }
 
