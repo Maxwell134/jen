776cd381fcff86c17043f55bb26d410906db789b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-05-29 12:32:10 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,8 +1,13 @@
-import groovy.json.JsonSlurperClassic 
-import groovy.json.JsonOutput 
+import groovy.json.JsonSlurper
 
 def sample(variable) {
-
-echo "Hello variable.'message'"
-
+    def jsonSlurper = new JsonSlurper()
+    def jsonObject = jsonSlurper.parseText(variable)
+    println "Hello ${jsonObject.message}"
 }
+
+// Sample JSON input as a string
+def jsonString = '{"message": "Hello World"}'
+
+// Call the sample function with the JSON string
+sample(jsonString)
